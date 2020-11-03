038f410f8d2cd254f2c1f43d9d0b5da864b77034
XiaoHuaShiFu <827032783@qq.com>
2020-11-03 20:52:17 +0800

    1

--- paperless-recruit/user/src/test/java/com/xiaohuashifu/recruit/user/service/UserServiceImplTest.java
+++ paperless-recruit/user/src/test/java/com/xiaohuashifu/recruit/user/service/UserServiceImplTest.java
@@ -3,16 +3,9 @@ package com.xiaohuashifu.recruit.user.service;
 import com.xiaohuashifu.recruit.api.service.UserService;
 import com.xiaohuashifu.recruit.common.pojo.dto.UserDTO;
 import com.xiaohuashifu.recruit.common.result.Result;
-import com.xiaohuashifu.recruit.user.UserApplication;
 import com.xiaohuashifu.recruit.user.UserApplicationTests;
 import org.apache.dubbo.config.annotation.Reference;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
 /**
  * 描述：
@@ -21,26 +14,26 @@ import static org.junit.jupiter.api.Assertions.*;
  * @email: 827032783@qq.com
  * @create: 2020/10/30 19:53
  */
-class UserServiceImplTest extends UserApplicationTests{
+public class UserServiceImplTest extends UserApplicationTests{
 
     @Reference
     private UserService userService;
 
     @Test
-    void getUser() {
+    public void getUser() {
         final Result<UserDTO> user = userService.getUser(1L);
         System.out.println(user);
     }
 
     @Test
-    void testGetUser() {
+    public void testGetUser() {
     }
 
     @Test
-    void saveUser() {
+    public void saveUser() {
     }
 
     @Test
-    void changeUser() {
+    public void changeUser() {
     }
 }

80e30462a5ec2903b957deada9d4147abf0e6108
XiaoHuaShiFu <827032783@qq.com>
2020-11-10 23:22:41 +0800

    Update MyAuthenticationFailureHandler.java

--- paperless-recruit/authentication/authentication-service/src/main/java/com/xiaohuashifu/recruit/authentication/service/oauth2/MyAuthenticationFailureHandler.java
+++ paperless-recruit/authentication/authentication-service/src/main/java/com/xiaohuashifu/recruit/authentication/service/oauth2/MyAuthenticationFailureHandler.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -26,7 +25,7 @@ public class MyAuthenticationFailureHandler implements AuthenticationFailureHand
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
-        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().write(exception.getMessage());
     }

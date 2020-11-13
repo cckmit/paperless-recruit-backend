d0fa03df5bc6a681a506993b845590466b485f36
XiaoHuaShiFu <827032783@qq.com>
2020-11-14 00:45:28 +0800

    1

--- paperless-recruit/authentication/authentication-service/src/main/java/com/xiaohuashifu/recruit/authentication/service/config/WebSecurityConfig.java
+++ paperless-recruit/authentication/authentication-service/src/main/java/com/xiaohuashifu/recruit/authentication/service/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -41,6 +42,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // 不校验改，否则资源服务器访问时报 403 错误
+        web.ignoring().antMatchers("/oauth/check_token");
+    }
+
     /**
      * Spring Security 安全配置
      *
@@ -50,23 +57,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                // 允许所有请求
-                .authorizeRequests()
-                .anyRequest()
-                .permitAll()
+                    // 当前SecurityFilterChain匹配那些请求，表示认证
+                    // 这里表示匹配任何请求（URL），表示都需要认证
+                    .requestMatchers()
+                    .anyRequest()
                 .and()
-                // 关闭csrf保护：跨站请求伪造（Cross-site request forgery）
-                .csrf().disable()
-                // 允许cors：跨域资源共享（Cross-origin resource sharing）
-                .cors()
+                    // 这里是FilterSecurityInterceptor拦截器，表示授权
+                    // 允许所有请求（URL），表示都不需要授权
+                    .authorizeRequests()
+                    .anyRequest()
+                    .permitAll()
                 .and()
-                // 关闭表单登录
-                .formLogin().disable()
-                // 关闭session，任何情况不创建Cookie
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    // 关闭csrf保护：跨站请求伪造（Cross-site request forgery）
+                    .csrf().disable()
+                    // 允许cors：跨域资源共享（Cross-origin resource sharing）
+                    .cors()
                 .and()
-                // 添加短信验证码认证配置
-                .apply(smsAuthenticationConfig);
+                    // 关闭session，任何情况不创建Cookie
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    // 添加短信验证码认证配置
+                    .apply(smsAuthenticationConfig);
     }
 
 }
--- paperless-recruit/authentication/authentication-service/src/main/java/com/xiaohuashifu/recruit/authentication/service/controller/UserController.java
+++ paperless-recruit/authentication/authentication-service/src/main/java/com/xiaohuashifu/recruit/authentication/service/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.xiaohuashifu.recruit.authentication.service.controller;
 
+import com.xiaohuashifu.recruit.common.validator.annotation.Id;
+import com.xiaohuashifu.recruit.user.api.service.UserService;
 import io.jsonwebtoken.Jwts;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.NotBlank;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -23,6 +30,9 @@ public class UserController {
     @Value("${jwt.signingKey}")
     private String signingKey;
 
+    @Reference
+    private UserService userService;
+
     @GetMapping("index")
     public Object index(Authentication authentication) {
         return authentication;
@@ -33,5 +43,15 @@ public class UserController {
         String authorization = request.getHeader("Authorization");
         return Jwts.parser().setSigningKey(signingKey.getBytes(StandardCharsets.UTF_8))
                 .parseClaimsJws(StringUtils.substringAfter(authorization, "bearer "));
+    }
+
+    @GetMapping("test1")
+    public Object test1(Long id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("test2")
+    public Object test2() {
+        return null;
     }
 }

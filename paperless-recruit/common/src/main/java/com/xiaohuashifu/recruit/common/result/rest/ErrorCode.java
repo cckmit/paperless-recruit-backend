55f78df05f46243a1d1d914f8e817b502d137e6b
XiaoHuaShiFu <827032783@qq.com>
2020-11-18 00:12:37 +0800

    1

--- paperless-recruit/common/src/main/java/com/xiaohuashifu/recruit/common/result/rest/ErrorCode.java
+++ paperless-recruit/common/src/main/java/com/xiaohuashifu/recruit/common/result/rest/ErrorCode.java
@@ -1,4 +1,6 @@
-package com.xiaohuashifu.recruit.common.result;
+package com.xiaohuashifu.recruit.common.result.rest;
+
+import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 
@@ -14,114 +16,118 @@ public enum ErrorCode implements Serializable {
     /**
      * 非法参数
      */
-    INVALID_PARAMETER(
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST,
             "InvalidParameter", "The required parameter is not valid."),
 
     /**
      * 参数为空，即字符串，集合等为null或者长度为0
      */
-    INVALID_PARAMETER_IS_EMPTY(
+    INVALID_PARAMETER_IS_EMPTY(HttpStatus.BAD_REQUEST,
             "InvalidParameter.IsEmpty", "The required parameter must be not empty."),
 
     /**
      * 参数为null
      */
-    INVALID_PARAMETER_IS_NULL(
+    INVALID_PARAMETER_IS_NULL(HttpStatus.BAD_REQUEST,
             "InvalidParameter.IsNull", "The required parameter must be not null."),
 
     /**
      * 字符串参数为NULL或者没有非空白字符
      */
-    INVALID_PARAMETER_IS_BLANK(
+    INVALID_PARAMETER_IS_BLANK(HttpStatus.BAD_REQUEST,
             "InvalidParameter.IsBlank", "The required parameter must be not blank."),
 
     /**
      * 参数值超过限定范围，Number类型
      */
-    INVALID_PARAMETER_VALUE_EXCEEDED(
+    INVALID_PARAMETER_VALUE_EXCEEDED(HttpStatus.BAD_REQUEST,
             "InvalidParameter.ValueExceeded", "The name of {Parameter} exceeded, max: {Value}."),
 
     /**
      * 参数值小于限定范围，Number类型
      */
-    INVALID_PARAMETER_VALUE_BELOW(
+    INVALID_PARAMETER_VALUE_BELOW(HttpStatus.BAD_REQUEST,
             "InvalidParameter.ValueBelow", "The name of {Parameter} below, min: {Value}."),
 
     /**
      * 参数值范围不正确
      */
-    INVALID_PARAMETER_VALUE(
+    INVALID_PARAMETER_VALUE(HttpStatus.BAD_REQUEST,
             "InvalidParameter.Value", "The parameter of {Parameter} is invalid."),
 
     /**
-     * 参数长度不在规定范围内，String或集合类型
+     * 参数长度不在规定范围内，String类型
      */
-    INVALID_PARAMETER_SIZE(
+    INVALID_PARAMETER_SIZE(HttpStatus.BAD_REQUEST,
             "InvalidParameter.Size", "The size of {Parameter} must be from {Min} to {Max}."),
 
     /**
      * 未知错误
      */
-    UNKNOWN_ERROR(
+    UNKNOWN_ERROR(HttpStatus.BAD_REQUEST,
             "UnknownError", "The request processing has failed due to some unknown exception."),
 
     /**
      * 短时间内请求过多
      */
-    THROTTLING(
+    THROTTLING(HttpStatus.BAD_REQUEST,
             "Throttling", "You have made too many requests within a short time."),
 
     /**
      * 未通过认证
      */
-    UNAUTHORIZED("Unauthorized", "User not authorized."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED,
+            "Unauthorized", "User not authorized."),
 
     /**
      * 没有附带请求该资源所需的token
      */
-    UNAUTHORIZED_TOKEN_IS_NULL(
+    UNAUTHORIZED_TOKEN_IS_NULL(HttpStatus.UNAUTHORIZED,
             "Unauthorized.TokenIsNull", "The request auth token is null."),
 
     /**
      * 被禁止
      */
-    FORBIDDEN(
+    FORBIDDEN(HttpStatus.FORBIDDEN,
             "Forbidden.SubUser", "Forbidden."),
 
     /**
      * 用户在未认证的情况下操作该资源
      */
-    FORBIDDEN_UNAUTHORIZED(
+    FORBIDDEN_UNAUTHORIZED(HttpStatus.FORBIDDEN,
             "Forbidden.Unauthorized", "User not authorized to operate on the specified resource."),
 
     /**
      * 用户在无权操作该资源
      */
-    FORBIDDEN_SUB_USER(
+    FORBIDDEN_SUB_USER(HttpStatus.FORBIDDEN,
             "Forbidden.SubUser", "The specified action is not available for you."),
 
     /**
      * 未找到该参数
      */
-    INVALID_PARAMETER_NOT_FOUND(
+    INVALID_PARAMETER_NOT_FOUND(HttpStatus.NOT_FOUND,
             "InvalidParameter.NotFound", "The specified resource does not exist."),
 
     /**
-     * 操作冲突
+     * 参数长度不在规定范围内，String类型
      */
-    OPERATION_CONFLICT(
+    OPERATION_CONFLICT(HttpStatus.CONFLICT,
             "OperationConflict", "Request was denied due to conflict with a previous request."),
 
     /**
      * 由于某些未知错误、异常或失败，请求处理失败。
      */
-    INTERNAL_ERROR("InternalError",
-            "The request processing has failed due to some unknown exception, exception or failure.");
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
+            "InternalError", "The request processing has failed due to some unknown exception, exception or failure.");
 
+
+    private final HttpStatus httpStatus;
     private final String code;
     private final String message;
 
-    ErrorCode(String code, String message) {
+    ErrorCode(HttpStatus httpStatus, String code, String message) {
+        this.httpStatus = httpStatus;
         this.code = code;
         this.message = message;
     }
@@ -134,10 +140,15 @@ public enum ErrorCode implements Serializable {
         return code;
     }
 
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
     @Override
     public String toString() {
         return "ErrorCode{" +
-                "code='" + code + '\'' +
+                "httpStatus=" + httpStatus +
+                ", code='" + code + '\'' +
                 ", message='" + message + '\'' +
                 '}';
     }
---
+++ paperless-recruit/common/src/main/java/com/xiaohuashifu/recruit/common/result/rest/Result.java
@@ -0,0 +1,155 @@
+package com.xiaohuashifu.recruit.common.result.rest;
+
+import java.io.Serializable;
+import java.text.MessageFormat;
+
+/**
+ * 描述: 请求结果封装
+ *
+ * @author xhsf
+ * @email 827032783@qq.com
+ * @create 2019-10-09
+ */
+public class Result<T> implements Serializable {
+
+    /**
+     * 调用是否成功
+     */
+    private Boolean success;
+
+    /**
+     * 业务数据
+     */
+    private T data;
+
+    /**
+     * 错误码，在出错时才会带上
+     */
+    private ErrorCode errorCode;
+
+    /**
+     * 错误简短信息
+     */
+    private Object message;
+
+    private Result(Boolean success) {
+        this.success = success;
+    }
+
+    /**
+     * 成功调用时的构造方法
+     *
+     * @return Result
+     */
+    public static <T> Result<T> success() {
+        return new Result<>(true);
+    }
+
+    /**
+     * 成功调用时的构造方法
+     *
+     * @param data 业务数据
+     * @return Result<T>
+     */
+    public static <T> Result<T> success(T data) {
+        final Result<T> result = new Result<>(true);
+        result.setData(data);
+        return result;
+    }
+
+    /**
+     * 失败调用时的构造方法
+     *
+     * @param errorCode 错误码
+     * @param message 错误简短信息
+     * @return Result<T>
+     */
+    public static <T> Result<T> fail(ErrorCode errorCode, Object message) {
+        final Result<T> result = new Result<>(false);
+        result.setErrorCode(errorCode);
+        result.setMessage(message);
+        return result;
+    }
+
+    /**
+     * 失败调用时的构造方法
+     *
+     * @param errorCode 错误码
+     * @param format 格式化字符串
+     * @param args 参数
+     * @return Result<T>
+     */
+    public static <T> Result<T> fail(ErrorCode errorCode, String format, Object... args) {
+        final Result<T> result = new Result<>(false);
+        result.setErrorCode(errorCode);
+        result.setMessage(MessageFormat.format(format, args));
+        return result;
+    }
+
+    /**
+     * 失败调用时的构造方法
+     *
+     * @param errorCode 错误码
+     * @return Result<T>
+     */
+    public static <T> Result<T> fail(ErrorCode errorCode) {
+        final Result<T> result = new Result<>(false);
+        result.setErrorCode(errorCode);
+        return result;
+    }
+
+    /**
+     * 失败调用时的构造方法
+     *
+     * @param result 结果
+     * @return Result<T1, T2>
+     */
+    public static <T1, T2> Result<T1> fail(Result<T2> result) {
+        final Result<T1> result1 = new Result<>(false);
+        result1.setErrorCode(result.getErrorCode());
+        result1.setMessage(result.getMessage());
+        return result1;
+    }
+
+    public Boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public Object getMessage() {
+        return message;
+    }
+
+    public void setMessage(Object message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "success=" + success +
+                ", data=" + data +
+                ", errorCode=" + errorCode +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}

6c24b3da63b32f391046ec00b2d011dec970b7b8
XiaoHuaShiFu <827032783@qq.com>
2020-11-24 21:06:32 +0800

    ·

--- paperless-recruit/user/user-service/src/main/java/com/xiaohuashifu/recruit/user/service/service/UserProfileServiceImpl.java
+++ paperless-recruit/user/user-service/src/main/java/com/xiaohuashifu/recruit/user/service/service/UserProfileServiceImpl.java
@@ -17,10 +17,6 @@ import org.apache.dubbo.config.annotation.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.stream.Collectors;
 

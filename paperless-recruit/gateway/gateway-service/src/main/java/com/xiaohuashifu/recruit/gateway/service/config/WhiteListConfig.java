e82492452647bd9d541a453093b00afbdbcb7134
XiaoHuaShiFu <827032783@qq.com>
2020-11-27 17:11:30 +0800

    Update WhiteListConfig.java

--- paperless-recruit/gateway/gateway-service/src/main/java/com/xiaohuashifu/recruit/gateway/service/config/WhiteListConfig.java
+++ paperless-recruit/gateway/gateway-service/src/main/java/com/xiaohuashifu/recruit/gateway/service/config/WhiteListConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
  * @author xhsf
  * @create 2020/11/27 16:45
  */
+// TODO: 2020/11/27 这里更好的做法是，把白名单放在数据库里，然后定时更新白名单，把允许的逻辑放在UrlAuthorityChecker中
 @Configuration
 @ConfigurationProperties(prefix = "white-list")
 public class WhiteListConfig {

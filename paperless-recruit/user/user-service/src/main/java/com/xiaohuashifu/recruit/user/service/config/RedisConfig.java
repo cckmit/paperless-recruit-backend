dcdb25c13e1500f6b69cbce58d16b045e6373519
XiaoHuaShiFu <827032783@qq.com>
2020-12-01 21:51:59 +0800

    1

--- paperless-recruit/user/user-service/src/main/java/com/xiaohuashifu/recruit/user/service/config/RedisConfig.java
+++ paperless-recruit/user/user-service/src/main/java/com/xiaohuashifu/recruit/user/service/config/RedisConfig.java
@@ -3,11 +3,8 @@ package com.xiaohuashifu.recruit.user.service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
-import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.scripting.support.ResourceScriptSource;
 
 /**
@@ -19,25 +16,11 @@ import org.springframework.scripting.support.ResourceScriptSource;
  */
 @Configuration
 public class RedisConfig {
-//    /**
-//     * RedisTemplate对象单例
-//     * @return RedisTemplate
-//     */
-//    @Bean
-//    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-//        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-//        RedisSerializer<String> stringSerializer = redisTemplate.getStringSerializer();
-//        // 设置字符串序列化器，这样Spring就会把Redis的key当成字符串处理
-//        redisTemplate.setKeySerializer(stringSerializer);
-//        redisTemplate.setValueSerializer(stringSerializer);
-//        redisTemplate.setHashKeySerializer(stringSerializer);
-//        redisTemplate.setHashValueSerializer(stringSerializer);
-//
-//        // redis连接工厂
-//        redisTemplate.setConnectionFactory(connectionFactory);
-//        return redisTemplate;
-//    }
 
+    /**
+     * 自增id Redis脚本
+     * @return 自增id值
+     */
     @Bean("incrementIdRedisScript")
     public RedisScript<Long> incrementIdRedisScript() {
         DefaultRedisScript<Long> incrementIdRedisScript = new DefaultRedisScript<>();

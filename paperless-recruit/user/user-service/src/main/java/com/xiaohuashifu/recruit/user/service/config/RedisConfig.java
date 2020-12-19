37940d356b2ae08cae74f78b9153d9cae24051b6
XiaoHuaShiFu <827032783@qq.com>
2020-12-20 01:28:16 +0800

    1

--- paperless-recruit/user/user-service/src/main/java/com/xiaohuashifu/recruit/user/service/config/RedisConfig.java
+++ paperless-recruit/user/user-service/src/main/java/com/xiaohuashifu/recruit/user/service/config/RedisConfig.java
@@ -5,11 +5,7 @@ import com.xiaohuashifu.recruit.common.limiter.frequency.FrequencyLimiter;
 import com.xiaohuashifu.recruit.common.limiter.frequency.FrequencyLimiterManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.data.redis.core.script.RedisScript;
-import org.springframework.scripting.support.ResourceScriptSource;
 
 /**
  * 描述：Redis 相关配置
@@ -19,24 +15,6 @@ import org.springframework.scripting.support.ResourceScriptSource;
  */
 @Configuration
 public class RedisConfig {
-
-    /**
-     * 自增 id lua 脚本 classpath 路径
-     */
-    private static final String INCREMENT_ID_REDIS_LUA_SCRIPT_CLASS_PATH = "/redis/lua/IncrementId.lua";
-
-    /**
-     * 自增 id Redis 脚本
-     * @return 自增 id 值
-     */
-    @Bean("incrementIdRedisScript")
-    public RedisScript<Long> incrementIdRedisScript() {
-        DefaultRedisScript<Long> incrementIdRedisScript = new DefaultRedisScript<>();
-        incrementIdRedisScript.setResultType(Long.class);
-        incrementIdRedisScript.setScriptSource(new ResourceScriptSource(
-                new ClassPathResource(INCREMENT_ID_REDIS_LUA_SCRIPT_CLASS_PATH)));
-        return incrementIdRedisScript;
-    }
 
     /**
      * 限频器

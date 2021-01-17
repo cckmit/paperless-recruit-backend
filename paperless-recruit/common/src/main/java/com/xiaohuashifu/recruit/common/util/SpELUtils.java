ece4f90cb8aba0bf1e774b9eb686e2f3ce5193f8
XiaoHuaShiFu <827032783@qq.com>
2021-01-17 23:54:38 +0800

    Update SpELUtils.java

--- paperless-recruit/common/src/main/java/com/xiaohuashifu/recruit/common/util/SpELUtils.java
+++ paperless-recruit/common/src/main/java/com/xiaohuashifu/recruit/common/util/SpELUtils.java
@@ -6,7 +6,6 @@ import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.ParserContext;
-import org.springframework.expression.common.TemplateParserContext;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 

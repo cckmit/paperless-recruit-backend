e1afdf71a416cea5104526ba3c0b644a18f11d8c
XiaoHuaShiFu <827032783@qq.com>
2021-02-17 02:20:41 +0800

    Update EmailConsumerConfig.java

--- paperless-recruit/external/external-service/src/main/java/com/xiaohuashifu/recruit/external/service/mq/EmailConsumerConfig.java
+++ paperless-recruit/external/external-service/src/main/java/com/xiaohuashifu/recruit/external/service/mq/EmailConsumerConfig.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.xiaohuashifu.recruit.common.exception.UnknownServiceException;
 import com.xiaohuashifu.recruit.external.api.request.CreateAndSendEmailAuthCodeRequest;
 import com.xiaohuashifu.recruit.external.api.request.SendSimpleEmailRequest;
+import com.xiaohuashifu.recruit.external.api.request.SendTemplateEmailRequest;
 import com.xiaohuashifu.recruit.external.api.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
@@ -42,11 +43,17 @@ public class EmailConsumerConfig {
     @Value("${rocketmq.consumer.group.send-simple-email}")
     private String sendSimpleEmailConsumerGroup;
 
+    @Value("${rocketmq.consumer.group.send-template-email}")
+    private String sendTemplateEmailConsumerGroup;
+
     @Value("${rocketmq.tags.create-and-send-email-auth-code}")
     private String createAndSendEmailAuthCodeTag;
 
     @Value("${rocketmq.tags.send-simple-email}")
     private String sendSimpleEmailTag;
+
+    @Value("${rocketmq.tags.send-template-email}")
+    private String sendTemplateEmailTag;
 
     /**
      * 创建并发送邮件验证码消费者，消费失败直接丢弃信息
@@ -100,6 +107,29 @@ public class EmailConsumerConfig {
     }
 
     /**
+     * 发送模板邮件消费者
+     *
+     * @return DefaultMQPushConsumer
+     * @throws MQClientException .
+     */
+    @Bean
+    public DefaultMQPushConsumer sendTemplateEmailConsumer() throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(sendTemplateEmailConsumerGroup);
+        consumer.setNamesrvAddr(nameServer);
+        consumer.subscribe(topic, sendTemplateEmailTag);
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            for (MessageExt msg : msgs) {
+                String msgString = new String(msg.getBody());
+                SendTemplateEmailRequest request = JSON.parseObject(msgString, SendTemplateEmailRequest.class);
+                emailService.sendTemplateEmail(request);
+            }
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        consumer.start();
+        return consumer;
+    }
+
+    /**
      * 关闭时：摧毁 Bean createAndSendEmailAuthCodeConsumer，sendSimpleEmailConsumer
      *
      * @return DisposableBean
@@ -109,6 +139,7 @@ public class EmailConsumerConfig {
         return ()-> {
             createAndSendEmailAuthCodeConsumer().shutdown();
             sendSimpleEmailConsumer().shutdown();
+            sendTemplateEmailConsumer().shutdown();
         };
     }
     

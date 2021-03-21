919b02fd69874566118a140af0971a59e0b6e200
XiaoHuaShiFu <827032783@qq.com>
2021-03-21 21:44:54 +0800

    1

--- paperless-recruit/facade/facade-service/src/main/java/com/xiaohuashifu/recruit/facade/service/assembler/ApplicationFormAssembler.java
+++ paperless-recruit/facade/facade-service/src/main/java/com/xiaohuashifu/recruit/facade/service/assembler/ApplicationFormAssembler.java
@@ -1,8 +1,12 @@
 package com.xiaohuashifu.recruit.facade.service.assembler;
 
+import com.xiaohuashifu.recruit.facade.service.assembler.translator.PathToUrl;
+import com.xiaohuashifu.recruit.facade.service.assembler.translator.UrlTranslator;
+import com.xiaohuashifu.recruit.facade.service.assembler.translator.impl.UrlTranslatorImpl;
 import com.xiaohuashifu.recruit.facade.service.vo.ApplicationFormVO;
 import com.xiaohuashifu.recruit.registration.api.dto.ApplicationFormDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * 描述：ApplicationForm 的装配器
@@ -10,9 +14,11 @@ import org.mapstruct.Mapper;
  * @author xhsf
  * @create 2021/1/9 13:02
  */
-@Mapper(componentModel = "spring")
+@Mapper(uses = UrlTranslatorImpl.class, componentModel = "spring")
 public interface ApplicationFormAssembler {
 
+    @Mapping(target = "avatarUrl", qualifiedBy = {UrlTranslator.class, PathToUrl.class})
+    @Mapping(target = "attachmentUrl", qualifiedBy = {UrlTranslator.class, PathToUrl.class})
     ApplicationFormVO applicationFormDTOToApplicationFormVO(ApplicationFormDTO applicationFormDTO);
 
 }
